2682e1aeaf87bb12e9a876e97a21a90db4107e84
mysteryman123456 <nschldhl61@gmail.com>
2024-12-14 20:22:17 +0545

    New assignment

---
+++ NetBeansProjects/JavaswigApp/src/javaswigapp/JavaswigApp.java
@@ -0,0 +1,16 @@
+package javaswigapp;
+
+import javaswigapp.View.SignupScreen;
+//import javaswigapp.View.StartScreen;
+
+public class JavaswigApp {
+public static void main(String[] args) {
+    
+//    StartScreen ss = new StartScreen();
+//    ss.setVisible(true);
+//    
+    SignupScreen ss = new SignupScreen();
+    ss.setVisible(true);
+     
+   }
+ }
